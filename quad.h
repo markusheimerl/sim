bf760966cb849d697c3b6c72a4441f073ccd1351
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2024-12-26 14:00:44 +0000

    Revert "Refactor drone control logic for improved clarity and organization"
    This reverts commit d554a93d822fd1532bdb32607813e99b18be07c8.

--- quad.h
+++ quad.h
@@ -162,122 +162,141 @@ void update_drone_physics(void) {
 }
 
 void update_drone_control(void) {
-    // 1. Position and Velocity Error Calculation
+    // --- LINEAR CONTROL ---
     double error_p[3], error_v[3];
     subVec3f(linear_position_W, linear_position_d_W, error_p);
     subVec3f(linear_velocity_W, linear_velocity_d_W, error_v);
 
-    // 2. Desired Force Calculation
-    double z_W_d[3];
-    // Combine position and velocity feedback
+    double z_W_d[3], temp[3];
     multScalVec3f(-k_p, error_p, z_W_d);
-    double temp[3];
     multScalVec3f(-k_v, error_v, temp);
     addVec3f(z_W_d, temp, z_W_d);
     
-    // Add gravity compensation and desired acceleration
     double gravity_term[3] = {0, M * G, 0};
     addVec3f(z_W_d, gravity_term, z_W_d);
-    multScalVec3f(M, linear_acceleration_d_W, temp);
-    addVec3f(z_W_d, temp, z_W_d);
-
-    // 3. Thrust Calculation
-    double y_body[3] = {0, 1, 0};
+    
+    double accel_term[3];
+    multScalVec3f(M, linear_acceleration_d_W, accel_term);
+    addVec3f(z_W_d, accel_term, z_W_d);
+    
     double z_W_B[3];
+    double y_body[3] = {0, 1, 0};
     multMatVec3f((double*)R_W_B, y_body, z_W_B);
+    
     double f_z_B_control = dotVec3f(z_W_d, z_W_B);
 
-    // 4. Desired Rotation Matrix Calculation
+    // --- ATTITUDE CONTROL ---
     double x_tilde_d_W[3] = {sin(yaw_d), 0, cos(yaw_d)};
-    double R_W_d[9];
     
-    // Calculate desired rotation matrix columns
     double temp_cross1[3], temp_cross2[3];
-    double R_W_d_col0[3], R_W_d_col1[3], R_W_d_col2[3];
+    double R_W_d_column_0[3], R_W_d_column_1[3], R_W_d_column_2[3];
     
     crossVec3f(z_W_d, x_tilde_d_W, temp_cross1);
     crossVec3f(temp_cross1, z_W_d, temp_cross2);
+    normVec3f(temp_cross2, R_W_d_column_0);
     
-    normVec3f(temp_cross2, R_W_d_col0);
-    normVec3f(temp_cross1, R_W_d_col1);
-    normVec3f(z_W_d, R_W_d_col2);
-    
-    // Assemble rotation matrix
-    for(int i = 0; i < 3; i++) {
-        R_W_d[i*3 + 0] = R_W_d_col1[i];
-        R_W_d[i*3 + 1] = R_W_d_col2[i];
-        R_W_d[i*3 + 2] = R_W_d_col0[i];
-    }
+    normVec3f(temp_cross1, R_W_d_column_1);
+    normVec3f(z_W_d, R_W_d_column_2);
 
-    // 5. Rotation Error Calculation
-    double R_W_d_T[9], R_W_B_T[9], temp_mat[9];
+    double R_W_d[9] = {
+        R_W_d_column_1[0], R_W_d_column_2[0], R_W_d_column_0[0],
+        R_W_d_column_1[1], R_W_d_column_2[1], R_W_d_column_0[1],
+        R_W_d_column_1[2], R_W_d_column_2[2], R_W_d_column_0[2]
+    };
+
+    double R_W_d_T[9], R_W_B_T[9];
     transpMat3f(R_W_d, R_W_d_T);
     transpMat3f((double*)R_W_B, R_W_B_T);
-    
-    double error_r[3];
-    multMat3f(R_W_d_T, (double*)R_W_B, temp_mat);
-    double temp_mat2[9];
+
+    double temp_mat1[9], temp_mat2[9], temp_mat3[9];
+    multMat3f(R_W_d_T, (double*)R_W_B, temp_mat1);
     multMat3f(R_W_B_T, R_W_d, temp_mat2);
-    subMat3f(temp_mat, temp_mat2, temp_mat);
-    so3vee(temp_mat, error_r);
+    subMat3f(temp_mat1, temp_mat2, temp_mat3);
+
+    double error_r[3];
+    so3vee(temp_mat3, error_r);
     multScalVec3f(0.5, error_r, error_r);
 
-    // 6. Angular Velocity Error Calculation
-    double error_w[3], temp_vec[3];
-    multMat3f(R_W_d_T, (double*)R_W_B, temp_mat);
-    multMatVec3f(temp_mat, angular_velocity_d_B, temp_vec);
+    double temp_vec[3], error_w[3];
+    multMat3f(R_W_d_T, (double*)R_W_B, temp_mat1);
+    multMatVec3f(temp_mat1, angular_velocity_d_B, temp_vec);
     subVec3f(angular_velocity_B, temp_vec, error_w);
 
-    // 7. Torque Control Calculation
     double tau_B_control[3];
     multScalVec3f(-k_R, error_r, tau_B_control);
-    multScalVec3f(-k_w, error_w, temp_vec);
-    addVec3f(tau_B_control, temp_vec, tau_B_control);
+    
+    double temp_vec2[3];
+    multScalVec3f(-k_w, error_w, temp_vec2);
+    addVec3f(tau_B_control, temp_vec2, tau_B_control);
 
-    // Add angular momentum compensation
-    double I_mat[9];
+    double I_mat[9], temp_vec3[3], temp_vec4[3];
     vecToDiagMat3f(I, I_mat);
-    multMatVec3f(I_mat, angular_velocity_B, temp_vec);
-    crossVec3f(angular_velocity_B, temp_vec, temp_vec);
-    addVec3f(tau_B_control, temp_vec, tau_B_control);
+    multMatVec3f(I_mat, angular_velocity_B, temp_vec3);
+    crossVec3f(angular_velocity_B, temp_vec3, temp_vec4);
+    addVec3f(tau_B_control, temp_vec4, tau_B_control);
 
-    // Add desired angular acceleration compensation
-    double term[3];
+    double term_0[3], term_1[3];
     multMatVec3f(R_W_d, angular_acceleration_d_B, temp_vec);
-    multMatVec3f(R_W_B_T, temp_vec, term);
-    
+    multMatVec3f(R_W_B_T, temp_vec, term_0);
+
     multMatVec3f(R_W_d, angular_velocity_d_B, temp_vec);
-    multMatVec3f(R_W_B_T, temp_vec, temp_vec);
-    crossVec3f(angular_velocity_B, temp_vec, temp_vec);
-    subVec3f(temp_vec, term, temp_vec);
-    
-    multMatVec3f(I_mat, temp_vec, temp_vec);
+    multMatVec3f(R_W_B_T, temp_vec, temp_vec2);
+    crossVec3f(angular_velocity_B, temp_vec2, term_1);
+
+    double temp_vec5[3];
+    subVec3f(term_1, term_0, temp_vec5);
+    multMatVec3f(I_mat, temp_vec5, temp_vec);
     subVec3f(tau_B_control, temp_vec, tau_B_control);
 
-    // 8. Motor Speed Calculation
-    double F_bar[16] = {
-        K_F,    K_F,    K_F,    K_F,
-        -K_F*L, K_F*L,  K_F*L,  -K_F*L,
-        K_M,    -K_M,   K_M,    -K_M,
-        K_F*L,  K_F*L,  -K_F*L, -K_F*L
+    // --- ROTOR SPEEDS ---
+    double F_bar[16];
+    // First row
+    F_bar[0] = K_F;
+    F_bar[1] = K_F;
+    F_bar[2] = K_F;
+    F_bar[3] = K_F;
+
+    // Calculate columns
+    double rotor_positions[4][3] = {
+        {-L, 0, L},
+        {L, 0, L},
+        {L, 0, -L},
+        {-L, 0, -L}
     };
+
+    for(int i = 0; i < 4; i++) {
+        double force[3] = {0, 1, 0};
+        double moment[3];
+        multScalVec3f(K_F, rotor_positions[i], temp_vec);
+        crossVec3f(temp_vec, force, moment);
+        
+        double column[3];
+        if(i % 2 == 0) {
+            column[0] = moment[0];
+            column[1] = K_M;
+            column[2] = moment[2];
+        } else {
+            column[0] = moment[0];
+            column[1] = -K_M;
+            column[2] = moment[2];
+        }
+        
+        F_bar[4 + i] = column[0];
+        F_bar[8 + i] = column[1];
+        F_bar[12 + i] = column[2];
+    }
 
     double F_bar_inv[16];
     inv4Mat4f(F_bar, F_bar_inv);
 
-    double control_input[4] = {
-        f_z_B_control,
-        tau_B_control[0],
-        tau_B_control[1],
-        tau_B_control[2]
-    };
-
+    double control_input[4] = {f_z_B_control, tau_B_control[0], tau_B_control[1], tau_B_control[2]};
     double omega_sign_square[4];
     multMatVec4f(F_bar_inv, control_input, omega_sign_square);
 
-    for(int i = 0; i < 4; i++) {
-        omega_control[i] = sqrt(fabs(omega_sign_square[i]));
-    }
+    omega_control[0] = sqrt(fabs(omega_sign_square[0]));
+    omega_control[1] = sqrt(fabs(omega_sign_square[1]));
+    omega_control[2] = sqrt(fabs(omega_sign_square[2]));
+    omega_control[3] = sqrt(fabs(omega_sign_square[3]));
 }
 
 void update_omega(void) {
