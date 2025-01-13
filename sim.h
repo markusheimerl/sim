586db0169bcf2a14bf1e940ada9ce867384e3b95
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2025-01-13 13:27:20 +0000

    Refactor simulation initialization to conditionally allocate resources based on render flag; improve memory management in free_sim function

--- sim.h
+++ sim.h
@@ -1,6 +1,7 @@
 #ifndef SIM_H
 #define SIM_H
 
+#include <stdbool.h>
 #include "rasterizer.h"
 #include "gif.h"
 #include "quad.h"
@@ -10,23 +11,23 @@ typedef struct {
     Quad* quad;
     uint8_t *frame_buffer;
     ge_GIF *gif;
+    bool render;
 } Sim;
 
-Sim* init_sim(){
+Sim* init_sim(bool render){
     Sim* sim = malloc(sizeof(Sim));
-    sim->meshes = malloc(2 * sizeof(Mesh*));
-    sim->meshes[0] = create_mesh("sim/rasterizer/drone.obj", "sim/rasterizer/drone.bmp");
-    sim->meshes[1] = create_mesh("sim/rasterizer/ground.obj", "sim/rasterizer/ground.bmp");
+    if(render){
+        sim->meshes = malloc(2 * sizeof(Mesh*));
+        sim->meshes[0] = create_mesh("sim/rasterizer/drone.obj", "sim/rasterizer/drone.bmp");
+        sim->meshes[1] = create_mesh("sim/rasterizer/ground.obj", "sim/rasterizer/ground.bmp");
+        sim->frame_buffer = calloc(WIDTH * HEIGHT * 3, sizeof(uint8_t));
+        char filename[100];
+        strftime(filename, 100, "%Y-%m-%d_%H-%M-%S_flight.gif", localtime(&(time_t){time(NULL)}));
+        sim->gif = ge_new_gif(filename, WIDTH, HEIGHT, 4, -1, 0);
+        transform_mesh(sim->meshes[1], (double[3]){0.0, -0.2, 0.0}, 1.0, (double[9]){1,0,0, 0,1,0, 0,0,1});
+    }
+    sim->render = render;
     sim->quad = init_quad(0.0, 0.0, 0.0);
-    sim->frame_buffer = calloc(WIDTH * HEIGHT * 3, sizeof(uint8_t));
-
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-    char filename[100];
-    sprintf(filename, "%d-%d-%d_%d-%d-%d_flight.gif", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-    sim->gif = ge_new_gif(filename, WIDTH, HEIGHT, 4, -1, 0);
-
-    transform_mesh(sim->meshes[1], (double[3]){0.0, -0.2, 0.0}, 1.0, (double[9]){1,0,0, 0,1,0, 0,0,1});
     return sim;
 }
 
@@ -39,10 +40,12 @@ void render_sim(Sim* sim){
 }
 
 void free_sim(Sim* sim){
-    free_meshes(sim->meshes, 2);
-    free(sim->frame_buffer);
-    ge_close_gif(sim->gif);
-    free(sim->gif);
+    if(sim->render){
+        free_meshes(sim->meshes, 2);
+        free(sim->frame_buffer);
+        ge_close_gif(sim->gif);
+        free(sim->gif);
+    }
     free(sim->quad);
     free(sim);
 }
